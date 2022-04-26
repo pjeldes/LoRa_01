0a96499a575ef3c96c1f7c3233dac9c6b4ef6597
pjeldes <pjeldes66@gmail.com>
2022-04-26 13:27:00 -0400

    consumo de corriente adecuado

--- src/main.c
+++ src/main.c
@@ -28,7 +28,7 @@ int main(void){
     E32.channel = LORA_CHANEL;
     E32.baudios = LORA_BAUDIOS_9600;
     E32.patity = LORA_PARITY_8N1;
-    E32.air_data_rate = LORA_AIR_DATA_RATE_1_2_K;
+    E32.air_data_rate = LORA_AIR_DATA_RATE_19_2_K;
     E32.fec_switch = LORA_FEC_SWITCH_OFF;
     E32.IO_drive_mode = LORA_IO_DRIVE_MODE_ON;
     E32.transmission_type = LORA_TRANSPARENT_TRANSMISSION_OFF;
@@ -37,9 +37,9 @@ int main(void){
 
     //lora set param
     lora_sleep_mode();
-    HAL_Delay(10);
+    HAL_Delay(30);
     lora_set_param(&UartONEConf_s,E32);
-    HAL_Delay(10);
+    HAL_Delay(30);
     lora_normal_mode();
   //mensaje del emisor
     while(1){
